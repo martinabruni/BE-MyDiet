b03b82641170d3adab4055d306041bcc723bd762
Martina Bruni <martina.bruni@agic.it>
2025-06-10 17:06:00 +0200

    feat(controllers): added UserController example

---
+++ src/Apis/MyDiet.Core.Api/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyDiet.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUser()
+        {
+            return Ok("Hello from UserController!");
+        }
+    }
+}
