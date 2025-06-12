568c1d288661302a81c3bd892d4769413a6661a4
Martina Bruni <martina.bruni@agic.it>
2025-06-12 14:55:41 +0200

    fix(jwt): refactor controller

--- src/Apis/MyDiet.Core.Api/Controllers/UserJwtTokenController.cs
+++ src/Apis/MyDiet.Core.Api/Controllers/UserJwtTokenController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyDiet.Core.Domain.Dtos;
 using MyDiet.Core.Domain.Interfaces;
+using System.Security.Cryptography;
 
 namespace MyDiet.Core.Api.Controllers
 {
@@ -8,29 +9,29 @@ namespace MyDiet.Core.Api.Controllers
     [Route("api/[controller]/[action]")]
     public class UserJwtTokenController : ControllerBase
     {
-        private readonly IJwtTokenService<UserClaimDto> _jwtTokenService;
+        private readonly IJwtTokenService<UserClaimDto, RSA> _jwtTokenService;
 
-        public UserJwtTokenController(IJwtTokenService<UserClaimDto> jwtTokenService)
+        public UserJwtTokenController(IJwtTokenService<UserClaimDto, RSA> jwtTokenService)
         {
             _jwtTokenService = jwtTokenService;
         }
 
         [HttpGet]
-        public IActionResult GetToken()
+        public async Task<IActionResult> GetPublicKey()
         {
-            try
+            var publicKey = await _jwtTokenService.GetPublicKey();
+            return Ok(new { PublicKey = Convert.ToBase64String(publicKey) });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetToken()
+        {
+            var userClaim = new UserClaimDto
             {
-                var userClaim = new UserClaimDto
-                {
-                    UserId = Guid.NewGuid() // This should be replaced with actual user ID retrieval logic
-                };
-                var token = _jwtTokenService.GenerateTokenAsync(userClaim).Result;
-                return Ok(new { Token = token });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
-            }
+                UserId = Guid.NewGuid() // This should be replaced with actual user ID retrieval logic
+            };
+            var token = await _jwtTokenService.GenerateTokenAsync(userClaim);
+            return Ok(new { Token = token });
         }
     }
 }
